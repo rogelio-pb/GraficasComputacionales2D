4c1fdf52879e36753a9f6ba9c7d725a7ff69a3ba
rogelio-pb <rogelio.pe.2899@gmail.com>
2026-05-18 09:16:18 -0600

    Se agrego Prerequisites
    Se agregaron las librerias en el archivo prerequisites

---
+++ ProyectoGraficas/ProyectoGraficas/include/Prerequisites.h
@@ -0,0 +1,14 @@
+ #pragma once
+
+//librerias
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <thread>
+#include <map>
+#include <fstream>
+#include <unordered_map>
+
+//third parties
+#include <SFML/Graphics.hpp>
