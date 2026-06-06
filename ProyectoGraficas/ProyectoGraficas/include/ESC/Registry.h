8432edce0af41b33e35bc2bdd7a4c72d3c9af21c
rogelio-pb <rogelio.pe.2899@gmail.com>
2026-06-06 17:52:00 -0600

    Add ECS Registry header and project entries
    Add a new ESC::Registry skeleton header that defines entity storage (m_entities, m_versions, m_freelist), a component pool map and a system vector. Also update the .vcxproj and .vcxproj.filters to include the new headers (ESC/Registry.h, ESC/System.h, ESC/View.h) so they are part of the project and organized in the ESC filter.

--- ProyectoGraficas/ProyectoGraficas/include/ESC/Registry.h
+++ ProyectoGraficas/ProyectoGraficas/include/ESC/Registry.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "ESC/Types.h"
+#include "ESC/ComponentPool.h"
+#include"ESC/View.h"
+
+namespace ESC {
+	class Registry {
+	private:
+		// entidades
+		std::vector<EntityID>		m_entities;
+		std::vector<EntityVersion>	m_versions;
+		std::queue<EntityIndex>		m_freelist;
+
+		std::unordered_map<ComponentTypeID, std::unique_ptr<IComonentPool>> m_componentPools;
+		 //sistemas 
+		std::vector<std::unique_ptr<System>>m_sistem;
+	};
+}
